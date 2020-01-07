1d640d28b9abd85c4cfa1c05fe2689cf193fbd4a
Divyaprabha M <dhivyaprabhamurugesan@gmail.com>
2020-01-07 22:11:13 +0530

    Updated detection.py
    Now arguments only return the selected classes

--- detection.py
+++ detection.py
@@ -7,35 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/14GlYEjw6lzcmWKAFkG5gYXLVF71tPrYm
 """
 
-import numpy as np
-import sys
-import tarfile
-import tensorflow as tf
-import zipfile
-
-from collections import defaultdict
-from io import StringIO
-from matplotlib import pyplot as plt
-from PIL import Image
-
-
-from utils import label_map_util
-
-from utils import visualization_utils as vis_util
-
-# Path to frozen detection graph. This is the actual model that is used for the object detection.
-PATH_TO_CKPT =  'ssd_mobilenet_RoadDamageDetector.pb' 
-
-# List of the strings that is used to add correct label for each box.
-PATH_TO_LABELS = 'crack_label_map.pbtxt'
-
-NUM_CLASSES = 8
-
-def load_image_into_numpy_array(image):
-  (im_width, im_height) = image.size
-  return np.array(image.getdata()).reshape(
-      (im_height, im_width, 3)).astype(np.uint8)
-
 def detect(test_image, plot_show = False):
   detection_graph = tf.Graph()
   with detection_graph.as_default():
@@ -48,7 +19,8 @@ def detect(test_image, plot_show = False):
   label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
   categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
   category_index = label_map_util.create_category_index(categories)
-  print(type(test_image))
+  cat = np.array(["D00","D01","D10","D11","D20","D40","D43","D44"])
+  
   #INPUT FILE
   if str(type(test_image))!="<class 'list'>":
     TEST_IMAGE_PATHS=[test_image]  
@@ -78,6 +50,16 @@ def detect(test_image, plot_show = False):
         (boxes, scores, classes, num) = sess.run(
             [detection_boxes, detection_scores, detection_classes, num_detections],
             feed_dict={image_tensor: image_np_expanded})
+        min_score_thresh = 0.4
+        
+        #Select the boxes based on threshold
+        sc = np.where(scores[0] > min_score_thresh)[0]
+        scores_sel = scores[0][sc]
+        boxes_sel = boxes[0][sc,:]
+        classes_ind = classes[0][sc]
+       
+        classes_names = [(cat[int(i-1)]) for i in classes_ind]
+        
         # Visualization of the results of a detection.
         vis_util.visualize_boxes_and_labels_on_image_array(
             image_np,
@@ -85,16 +67,16 @@ def detect(test_image, plot_show = False):
             np.squeeze(classes).astype(np.int32),
             np.squeeze(scores),
             category_index,
-            min_score_thresh=0.3,
+            min_score_thresh=min_score_thresh,
             use_normalized_coordinates=True,
             line_thickness=8)
         if plot_show == True:
           plt.figure(figsize=IMAGE_SIZE)
           plt.imshow(image_np)
         final_images.append(image_np)
-        boxes_collect.append(boxes)
-        scores_collect.append(scores)
-        classes_collect.append(classes)
+        boxes_collect.append(boxes_sel)
+        scores_collect.append(scores_sel)
+        classes_collect.append(classes_names)
         num_collect.append(num)
-
-  return image_np,boxes_collect,scores_collect,classes_collect,num_collect 
+  
+  return image_np,boxes_collect,scores_collect,classes_collect,test_image
